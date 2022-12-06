d3b899ca3dca85fbbbf9ad7291ad971b60d8c91a
Tuhin <nazmulislam640050@gmail.com>
2022-12-07 01:47:18 +0600

    card section

--- src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,14 +1,39 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOptions from './AppointmentOptions';
 
-const AvailableAppointments = ({selectedDate}) => {
+
+  const AvailableAppointments = ({ selectedDate }) => {
+    const [AppointmentOption, setAppointmentOption] = useState([])
+    useEffect(() => {
+        fetch('appointmentOption.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOption(data))
+    }, [])
     return (
-        //<div>
-        
-            //<p className="text-center font-bold">AvailableAppointments on {format(selectedDate, 'PP')} </p>
-        //</div>
-        <div></div>
+        <section className='my-16'>
+            <p className="text-center font-bold">AvailableAppointments on {format(selectedDate, 'PP')} </p>
+
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                
+                {
+                    AppointmentOption.map(option => <AppointmentOptions
+                        key={option._id}
+                        appointmentOption={option}
+                    ></AppointmentOptions>)
+
+                }
+            </div>
+        </section>
+
     );
 };
 
 export default AvailableAppointments;
+
+
+
+
+
+
+
